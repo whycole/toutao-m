e965568cbea82ae71cc8c75dceb9e56d87de6c09
whycole <1940096457@qq.com>
2020-10-28 13:53:49 +0800

    '项目初始化-调整初始'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+//自动设置REM基准值(html标签字体大小)
+import 'amfe-flexible'
+
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
