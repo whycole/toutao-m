6b6acec2fd11bb78e4c7d0f2c5006343b72ceb44
whycole <1940096457@qq.com>
2020-11-03 12:35:11 +0800

    '...'

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,13 +1,158 @@
 <template>
-    <div class="my-container">我的页面</div>
+    <div class="my-container">
+      <van-cell-group v-if="user" class="my-info">
+        <van-cell class="base-info" title="单元格" value="内容" center :border="false">
+          <van-image class="avatar" slot="icon" round fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+          <div class="name" slot="title">昵称</div>
+          <van-button class="update-btn" size="small" round>编辑资料</van-button>
+        </van-cell>
+        <van-grid class="data-info" :border="false">
+          <van-grid-item class="data-info-item">
+            <div class="text-wrap" slot="text">
+              <div class="count">0</div>
+              <div class="text">头条</div>
+            </div>
+          </van-grid-item>
+          <van-grid-item class="data-info-item">
+            <div class="text-wrap" slot="text">
+              <div class="count">0</div>
+              <div class="text">关注</div>
+            </div>
+          </van-grid-item>
+          <van-grid-item class="data-info-item">
+            <div class="text-wrap" slot="text">
+              <div class="count">0</div>
+              <div class="text">粉丝</div>
+            </div>
+          </van-grid-item>
+          <van-grid-item class="data-info-item">
+            <div class="text-wrap" slot="text">
+              <div class="count">0</div>
+              <div class="text">获赞</div>
+            </div>
+          </van-grid-item>
+        </van-grid>
+      </van-cell-group>
+
+      <div v-else class="not-login">
+        <div @click="$router.push('/login')">
+          <img class="mobile" src="../../assets/手机.png">
+        </div>
+        <div class="texts">登录 / 注册</div>
+      </div>
+
+      <van-grid class="nav-grid mb-4" :column-num="2">
+        <van-grid-item class="nav-grid-item" icon-prefix="toutiao" icon="shoucang" text="收藏" />
+        <van-grid-item class="nav-grid-item" icon-prefix="toutiao" icon="lishi" text="历史" />
+      </van-grid>
+
+      <van-cell title="消息通知" is-link to="/" />
+      <van-cell class="mb-4" title="小智同学" is-link to="/" />
+      <van-cell v-if="user" class="logout-cell" title="退出登录" center/>
+    </div>
 </template>
 
 <script>
+    import { mapState } from 'vuex'
     export default {
-        name: "MyIndex"
+      name: "MyIndex",
+      computed: {
+        ...mapState(['user'])
+      }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.my-container{
+  .my-info{
+    background: url("../../assets/banner.png") no-repeat;
+    background-size: cover;
+    .base-info{
+      box-sizing: border-box;
+      height: 115px;
+      background: unset;
+      padding-top: 38px;
+      padding-bottom: 11px;
+      .avatar{
+        width: 66px;
+        height: 66px;
+        border: 1px solid #fff;
+        box-sizing: border-box;
+        margin-right: 11px;
+      }
+      .name{
+        color: #fff;
+        font-size: 15px;
+      }
+      .update-btn{
+        height: 16px;
+        font-size: 10px;
+        color: #666666;
+      }
+    }
+    .data-info{
+      .data-info-item{
+        height: 65px;
+        color: #fff;
+        .text-wrap{
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          .count{
+            font-size: 18px;
+          }
+          .text{
+            font-size: 11px;
+          }
+        }
+      }
+    }
+    /deep/ .van-grid-item__content{
+      background: unset;
+    }
+  }
+  .not-login{
+    height: 180px;
+    background: url("../../assets/banner.png") no-repeat;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .mobile{
+      width: 66px;
+      height: 66px;
+    }
+    .texts{
+      color: #fff;
+      font-size: 14px;
+    }
+  }
+  /deep/.nav-grid{
+    .nav-grid-item{
+      height: 70px;
+      .toutiao{
+        font-size: 22px;
+      }
+      .toutiao-shoucang{
+        color: #eb5253;
+      }
+      .toutiao-lishi{
+        color: #ff9d1d;
+      }
+      .van-grid-item__text{
+        font-size: 14px;
+        color: #333;
+      }
+    }
+  }
+  .logout-cell{
+    text-align: center;
+    color: #d86262;
+  }
+  .mb-4 {
+    margin-bottom: 4px;
+  }
+}
 </style>
