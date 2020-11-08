dc7467f45aee1a48701efe2159f308b7d6687a96
whycole <1940096457@qq.com>
2020-11-08 13:55:24 +0800

    'b'

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="search-suggestion-container">
-      <van-cell icon="search" :title="str" v-for="(str,index) in suggestions" :key="index"></van-cell>
+      <van-cell icon="search" v-for="(str,index) in suggestions" :key="index">
+        <div slot="title" v-html="highlight(str)"></div>
+      </van-cell>
     </div>
 </template>
 
 <script>
     import { getSearchSuggestions } from '../../../api/search'
+    import { debounce } from 'lodash'
     export default {
       name: "search-suggestion",
       props: {
@@ -16,7 +19,8 @@
       },
       data() {
         return {
-          suggestions: [] //联想建议数据列表
+          suggestions: [], //联想建议数据列表
+          htmlStr: ' <span style="color: red"></span>'
         }
       },
       watch: {
@@ -27,15 +31,20 @@
 
         //这才是监视的完整写法
         searchText: {
-            //当数据发生变化则会立即执行 handler 处理函数
-          async handler() {
+          //当数据发生变化则会立即执行 handler 处理函数
+          handler: debounce(async function() {
             //找到数据接口
             //请求获取数据
             //模板绑定展示
             const { data } = await getSearchSuggestions(this.searchText)
             this.suggestions = data.data.options
-          },
+          },200),
           immediate: true //该回调将会在侦听开始之后立即调用
+        }
+      },
+      methods: {
+        highlight(str) {
+          return str.replace(new RegExp(this.searchText,'gi'),`<span style="color: red">${this.searchText}</span>`)
         }
       }
     }
